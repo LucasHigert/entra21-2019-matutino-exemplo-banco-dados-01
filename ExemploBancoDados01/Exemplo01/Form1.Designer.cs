a87e38910f7c4bd694fb9d86a333aec69557214d
Lucas Sembarski Higert <lucassembarskihigert@gmail.com>
2019-06-12 11:27:26 -0300

    Desenvolvimento final CRUD banco

--- ExemploBancoDados01/Exemplo01/Form1.Designer.cs
+++ ExemploBancoDados01/Exemplo01/Form1.Designer.cs
@@ -41,6 +41,8 @@
             this.ColumnId = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ColumnModelo = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.btnApagar = new System.Windows.Forms.Button();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.lblId = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.nudAno)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvCarros)).BeginInit();
             this.SuspendLayout();
@@ -48,7 +50,7 @@
             // lblModelo
             // 
             this.lblModelo.AutoSize = true;
-            this.lblModelo.Location = new System.Drawing.Point(12, 9);
+            this.lblModelo.Location = new System.Drawing.Point(16, 98);
             this.lblModelo.Name = "lblModelo";
             this.lblModelo.Size = new System.Drawing.Size(42, 13);
             this.lblModelo.TabIndex = 0;
@@ -57,16 +59,16 @@
             // 
             // txtModelo
             // 
-            this.txtModelo.Location = new System.Drawing.Point(12, 35);
+            this.txtModelo.Location = new System.Drawing.Point(16, 124);
             this.txtModelo.Name = "txtModelo";
-            this.txtModelo.Size = new System.Drawing.Size(233, 20);
+            this.txtModelo.Size = new System.Drawing.Size(140, 20);
             this.txtModelo.TabIndex = 1;
             this.txtModelo.TextChanged += new System.EventHandler(this.txtModelo_TextChanged);
             // 
             // lblCor
             // 
             this.lblCor.AutoSize = true;
-            this.lblCor.Location = new System.Drawing.Point(12, 79);
+            this.lblCor.Location = new System.Drawing.Point(16, 168);
             this.lblCor.Name = "lblCor";
             this.lblCor.Size = new System.Drawing.Size(23, 13);
             this.lblCor.TabIndex = 2;
@@ -82,16 +84,16 @@
             "Prata",
             "Amarelo",
             "Azul"});
-            this.cbCor.Location = new System.Drawing.Point(12, 108);
+            this.cbCor.Location = new System.Drawing.Point(16, 197);
             this.cbCor.Name = "cbCor";
-            this.cbCor.Size = new System.Drawing.Size(233, 21);
+            this.cbCor.Size = new System.Drawing.Size(140, 21);
             this.cbCor.TabIndex = 4;
             this.cbCor.SelectedIndexChanged += new System.EventHandler(this.cbCor_SelectedIndexChanged);
             // 
             // lblPreco
             // 
             this.lblPreco.AutoSize = true;
-            this.lblPreco.Location = new System.Drawing.Point(9, 146);
+            this.lblPreco.Location = new System.Drawing.Point(13, 235);
             this.lblPreco.Name = "lblPreco";
             this.lblPreco.Size = new System.Drawing.Size(35, 13);
             this.lblPreco.TabIndex = 5;
@@ -101,7 +103,7 @@
             // lblAno
             // 
             this.lblAno.AutoSize = true;
-            this.lblAno.Location = new System.Drawing.Point(9, 213);
+            this.lblAno.Location = new System.Drawing.Point(13, 302);
             this.lblAno.Name = "lblAno";
             this.lblAno.Size = new System.Drawing.Size(26, 13);
             this.lblAno.TabIndex = 6;
@@ -110,7 +112,7 @@
             // 
             // nudAno
             // 
-            this.nudAno.Location = new System.Drawing.Point(12, 251);
+            this.nudAno.Location = new System.Drawing.Point(16, 340);
             this.nudAno.Maximum = new decimal(new int[] {
             2019,
             0,
@@ -122,7 +124,7 @@
             0,
             0});
             this.nudAno.Name = "nudAno";
-            this.nudAno.Size = new System.Drawing.Size(233, 20);
+            this.nudAno.Size = new System.Drawing.Size(140, 20);
             this.nudAno.TabIndex = 7;
             this.nudAno.Value = new decimal(new int[] {
             1950,
@@ -133,17 +135,17 @@
             // 
             // mtbPreco
             // 
-            this.mtbPreco.Location = new System.Drawing.Point(12, 172);
+            this.mtbPreco.Location = new System.Drawing.Point(16, 261);
             this.mtbPreco.Name = "mtbPreco";
-            this.mtbPreco.Size = new System.Drawing.Size(233, 20);
+            this.mtbPreco.Size = new System.Drawing.Size(140, 20);
             this.mtbPreco.TabIndex = 8;
             this.mtbPreco.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.mtbPreco_MaskInputRejected);
             // 
             // btnSalvar
             // 
-            this.btnSalvar.Location = new System.Drawing.Point(15, 290);
+            this.btnSalvar.Location = new System.Drawing.Point(19, 380);
             this.btnSalvar.Name = "btnSalvar";
-            this.btnSalvar.Size = new System.Drawing.Size(158, 105);
+            this.btnSalvar.Size = new System.Drawing.Size(65, 56);
             this.btnSalvar.TabIndex = 9;
             this.btnSalvar.Text = "Salvar";
             this.btnSalvar.UseVisualStyleBackColor = true;
@@ -157,13 +159,14 @@
             this.dgvCarros.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.ColumnId,
             this.ColumnModelo});
-            this.dgvCarros.Location = new System.Drawing.Point(297, 42);
+            this.dgvCarros.Location = new System.Drawing.Point(297, 43);
             this.dgvCarros.Name = "dgvCarros";
             this.dgvCarros.ReadOnly = true;
             this.dgvCarros.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvCarros.Size = new System.Drawing.Size(362, 244);
+            this.dgvCarros.Size = new System.Drawing.Size(347, 243);
             this.dgvCarros.TabIndex = 10;
             this.dgvCarros.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCarros_CellContentClick);
+            this.dgvCarros.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCarros_CellDoubleClick);
             // 
             // ColumnId
             // 
@@ -179,19 +182,39 @@
             // 
             // btnApagar
             // 
-            this.btnApagar.Location = new System.Drawing.Point(208, 292);
+            this.btnApagar.Location = new System.Drawing.Point(212, 382);
             this.btnApagar.Name = "btnApagar";
-            this.btnApagar.Size = new System.Drawing.Size(158, 105);
+            this.btnApagar.Size = new System.Drawing.Size(65, 56);
             this.btnApagar.TabIndex = 11;
             this.btnApagar.Text = "Apagar";
             this.btnApagar.UseVisualStyleBackColor = true;
             this.btnApagar.Click += new System.EventHandler(this.btnApagar_Click);
             // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(16, 42);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(40, 13);
+            this.lblCodigo.TabIndex = 12;
+            this.lblCodigo.Text = "Código";
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(107, 42);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(13, 13);
+            this.lblId.TabIndex = 13;
+            this.lblId.Text = "0";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(785, 450);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.lblCodigo);
             this.Controls.Add(this.btnApagar);
             this.Controls.Add(this.dgvCarros);
             this.Controls.Add(this.btnSalvar);
@@ -228,6 +251,8 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn ColumnId;
         private System.Windows.Forms.DataGridViewTextBoxColumn ColumnModelo;
         private System.Windows.Forms.Button btnApagar;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.Label lblId;
     }
 }
 
--- ExemploBancoDados01/Exemplo01/Form1.cs
+++ ExemploBancoDados01/Exemplo01/Form1.cs
@@ -20,6 +20,49 @@ namespace Exemplo01
 
         private void btnSalvar_Click(object sender, EventArgs e)
         {
+            if(lblId.Text=="0")
+            {
+                Inserir(); 
+
+            }
+            else
+            {
+                Alterar();
+            }
+        }
+        private void Alterar()
+        {
+            Carro carro = new Carro();
+            carro.Id = Convert.ToInt32(lblId.Text);
+            carro.Modelo = txtModelo.Text;
+            carro.Cor = cbCor.SelectedItem.ToString();
+            carro.Ano = Convert.ToInt32(nudAno.Value);
+            carro.Preco = Convert.ToDecimal(mtbPreco.Text);
+
+            SqlConnection conexao = new SqlConnection();
+            conexao.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=T:\Documentos\MeusCarros.mdf;Integrated Security=True;Connect Timeout=30";
+            conexao.Open();
+
+            SqlCommand comando = new SqlCommand();
+            comando.Connection = conexao;
+            comando.CommandText = @"UPDATE carros SET
+modelo=@Modelo,
+cor=@COR,
+ano=@ANO,
+preco=@PRECO
+WHERE id=@ID";
+            comando.Parameters.AddWithValue("@ID", carro.Id);
+            comando.Parameters.AddWithValue("@COR", carro.Cor);
+            comando.Parameters.AddWithValue("@MODELO", carro.Modelo);
+            comando.Parameters.AddWithValue("@ANO", carro.Ano);
+            comando.Parameters.AddWithValue("@PRECO", carro.Preco);
+            comando.ExecuteNonQuery();
+            conexao.Close();
+            AtualizarTabela();
+            LimparCampos();
+        }
+        private void Inserir()
+        {
             Carro carro = new Carro();
             carro.Modelo = txtModelo.Text;
             carro.Ano = Convert.ToInt32(nudAno.Value);
@@ -49,6 +92,7 @@ VALUES(@MODELO,@ANO,@PRECO,@COR)";
         }
         private void LimparCampos()
         {
+            lblId.Text = "0";
             txtModelo.Clear();
             nudAno.Value = DateTime.Now.Year;
             cbCor.SelectedIndex = -1;
@@ -157,6 +201,39 @@ VALUES(@MODELO,@ANO,@PRECO,@COR)";
                 conexao.Close();
                 AtualizarTabela();
             }
+        }
+
+        private void dgvCarros_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int id = Convert.ToInt32(dgvCarros.CurrentRow.Cells[0].Value);
+
+            SqlConnection conexao = new SqlConnection();
+            conexao.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=T:\Documentos\MeusCarros.mdf;Integrated Security=True;Connect Timeout=30";
+
+            conexao.Open();
+
+            SqlCommand comando = new SqlCommand();
+            comando.CommandText = @"SELECT id,modelo,preco,cor,ano FROM carros WHERE id=@ID";
+            comando.Parameters.AddWithValue("@ID", id);
+            comando.Connection = conexao;
+
+            DataTable tabela = new DataTable();
+            tabela.Load(comando.ExecuteReader());
+            DataRow linha = tabela.Rows[0];
+            Carro carro = new Carro();
+            carro.Id = Convert.ToInt32(linha["id"]);
+            carro.Modelo = linha["modelo"].ToString();
+            carro.Ano = Convert.ToInt32(linha["ano"]);
+            carro.Cor = linha["cor"].ToString();
+            carro.Preco = Convert.ToDecimal(linha["preco"]);
+
+            lblId.Text = carro.Id.ToString();
+            txtModelo.Text = carro.Modelo;
+            cbCor.SelectedItem = carro.Cor;
+            nudAno.Value = carro.Ano;
+            mtbPreco.Text = carro.Preco.ToString();
+            conexao.Close();
+            conexao.Close();
         }
     }
 }
